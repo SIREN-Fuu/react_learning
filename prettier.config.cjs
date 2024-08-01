8b09dceacbd02878f2f47cbc779fa1e58211419e
SIREN <19581158+SIREN-Fuu@users.noreply.github.com>
2024-08-01 11:35:26 +0900

    fix

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,6 +2,9 @@
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
+
+// https://zenn.dev/shimakaze_soft/articles/57642e22124968
+
 module.exports = {
   // 行の最大長
   printWidth: 80,
@@ -18,6 +21,9 @@ module.exports = {
   // シングルクォートを使用する
   singleQuote: true,
 
+  //必要な場合にのみ、オブジェクトプロパティを引用符で囲む(デフォルト)
+  quoteProps: 'as-needed',
+
   // JSX内ではダブルクォートを使用する
   jsxSingleQuote: false,
 
@@ -33,8 +39,17 @@ module.exports = {
   // アロー関数のパラメータに1つの引数がある場合でも括弧を常に付ける
   arrowParens: 'always',
 
+  //Print Widthで指定した値を超える時は折り返す(デフォルト)
+  proseWrap: 'preserve',
+
+  //displayプロパティのデフォルト値を尊重(デフォルト)
+  htmlWhitespaceSensitivity: 'css',
+
   // ファイルの末尾に改行を追加する
-  //   endOfLine: 'lf',
+  endOfLine: 'lf',
+
+  //埋め込まれたコードを自動的にフォーマットしない
+  embeddedLanguageFormatting: 'off',
 
   // プラグインの設定（必要に応じて追加）
   //   plugins: [require('prettier-plugin-tailwindcss')],
