e97e0ffb3e810b349eeba935f117b0419bd348cf
SIREN <19581158+SIREN-Fuu@users.noreply.github.com>
2024-08-01 14:11:42 +0900

    ESLint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,23 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es2020: true,
+    jest: true,
   },
   extends: ['plugin:react/recommended', 'eslint:recommended', 'prettier'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '**/*.html'],
   parserOptions: {
     ecmaVersion: 2020,
-    // ecmaVersion: 13,
-    sourceType: 'module',
   },
   parser: '@typescript-eslint/parser',
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-  ignorePatterns: ['**/*.html'],
+  settings: {
+    react: {
+      version: 'detect', // Reactのバージョンを自動検出
+    },
+  },
 };
