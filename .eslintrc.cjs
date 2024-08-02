aca2445bbd2b19deb5823c3c0d41fd251c8ca270
SIREN <19581158+SIREN-Fuu@users.noreply.github.com>
2024-08-02 11:58:53 +0900

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,8 +14,6 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
